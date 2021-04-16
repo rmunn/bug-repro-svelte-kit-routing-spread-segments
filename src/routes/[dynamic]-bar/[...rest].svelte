bed6914c696e1c91a212c5377d90c03f09fb01be
Robin Munn <rmunn@pobox.com>
2021-04-16 11:21:36 +0700

    Add routes reproducing bug

---
+++ src/routes/[dynamic]-bar/[...rest].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+    export function load({ page }) {
+        console.log(page);
+        return {
+            props: { params: page.params }
+        }
+    }
+</script>
+
+<script>
+    export let params;
+</script>
+
+<p>
+    Dynamic: {params.dynamic}<br/>
+    Rest: {params.rest}
+</p>
---
+++ src/routes/foo/[...rest]/index.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+    export function load({ page }) {
+        console.log(page);
+        return {
+            props: { params: page.params }
+        }
+    }
+</script>
+
+<script>
+    export let params;
+</script>
+
+<p>
+    Rest: {params.rest}
+</p>
